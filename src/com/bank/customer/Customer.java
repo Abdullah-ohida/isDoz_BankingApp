c0edfb77bcd6e46796561fe46006b262f1015956
Abdullah Ismail <ohida2001@gmail.com>
2021-02-11 21:10:35 +0100

    merge conflict resolve

--- src/com/bank/customer/Customer.java
+++ src/com/bank/customer/Customer.java
@@ -2,13 +2,7 @@ package com.bank.customer;
 
 
 import com.bank.account.Account;
-<<<<<<< HEAD
-=======
 import com.bank.account.AccountType;
-
-import java.util.ArrayList;
->>>>>>> local
-
 import java.util.ArrayList;
 
 public class Customer {
@@ -19,11 +13,8 @@ public class Customer {
     private String address;
     private String phoneNumber;
     private String gender;
-<<<<<<< HEAD
     private ArrayList<Account> accounts;
-=======
     private Account account;
->>>>>>> local
 
     public Customer(String firstName, String lastName, String dateOfBirth, String occupation, String address, String phoneNumber, String gender, String accountType) {
         this.firstName = firstName;
@@ -33,14 +24,12 @@ public class Customer {
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.gender = gender;
-<<<<<<< HEAD
         accounts = new ArrayList<>();
-=======
+
         if(accountType.equals("savings"))
             this.account = new Account(AccountType.SAVINGS);
         else
             this.account = new Account(AccountType.CURRENT);
->>>>>>> local
     }
     
     
